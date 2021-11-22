8323b581b2c0cc262756f3d19fe11d7ad94234d9
Andrey Minin <ewkjf@mail.ru>
2021-11-22 19:09:27 +0300

    added properties

--- src/main/java/io/blindet/file/storage/FileStorageApplication.java
+++ src/main/java/io/blindet/file/storage/FileStorageApplication.java
@@ -1,9 +1,12 @@
 package io.blindet.file.storage;
 
+import io.blindet.file.storage.properties.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FileStorageApplication {
 
 	public static void main(String[] args) {
