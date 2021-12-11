88416e980154faf790a872c20ff49d87b2805fd6
Andrey Minin <ewkjf@mail.ru>
2021-12-11 23:49:28 +0300

    prepared code to add tests

---
+++ src/test/java/com/onlym/file/storage/service/FileSystemStorageServiceTest.java
@@ -0,0 +1,42 @@
+package com.onlym.file.storage.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSystemStorageServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void store() {
+    }
+
+    @Test
+    void loadAllFilenamesInUploadDir() {
+    }
+
+    @Test
+    void loadPathByFilename() {
+    }
+
+    @Test
+    void loadFileByFilename() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
