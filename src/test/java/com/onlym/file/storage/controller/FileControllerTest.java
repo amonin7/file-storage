105271132e71fb626a442a9745253eba54e9f403
Andrey Minin <ewkjf@mail.ru>
2021-12-13 23:21:46 +0300

    added test class for fileController

---
+++ src/test/java/com/onlym/file/storage/controller/FileControllerTest.java
@@ -0,0 +1,25 @@
+package com.onlym.file.storage.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void downloadFile() {
+    }
+
+    @Test
+    void deleteAllFiles() {
+    }
+
+    @Test
+    void uploadMultipleFiles() {
+    }
+}
