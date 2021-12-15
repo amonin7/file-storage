e13efc81e50d48fefc763423eb063e2ffa9bace7
Andrey Minin <ewkjf@mail.ru>
2021-12-15 23:52:15 +0300

    started adding tests for the FileController

--- src/test/java/com/onlym/file/storage/controller/FileControllerTest.java
+++ src/test/java/com/onlym/file/storage/controller/FileControllerTest.java
@@ -1,11 +1,17 @@
 package com.onlym.file.storage.controller;
 
+import com.onlym.file.storage.service.StorageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class FileControllerTest {
+
+    private final StorageService storageService;
+
+    FileControllerTest(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @BeforeEach
     void setUp() {
