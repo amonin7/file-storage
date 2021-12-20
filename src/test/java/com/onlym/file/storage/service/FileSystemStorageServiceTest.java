c92063ff97ff9c2a0dadf5dbd820fb0d1f32b0d7
Andrey Minin <ewkjf@mail.ru>
2021-12-20 19:26:24 +0300

    added storeTest

--- src/test/java/com/onlym/file/storage/service/FileSystemStorageServiceTest.java
+++ src/test/java/com/onlym/file/storage/service/FileSystemStorageServiceTest.java
@@ -1,27 +1,47 @@
 package com.onlym.file.storage.service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.onlym.file.storage.properties.StorageProperties;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class FileSystemStorageServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    public static final String FILENAME = "file.txt";
+    public static final String CONTENT_TYPE = "file.txt";
+    public static final String FILE_CONTENT = "this is an example of txt test file";
 
-    @AfterEach
-    void tearDown() {
-    }
+    @Autowired
+    private FileSystemStorageService storageService;
 
-    @Test
-    void init() {
-    }
+    @Autowired
+    private StorageProperties properties;
 
     @Test
-    void store() {
+    void store() throws IOException {
+        storeTestFile();
+        String pathString = getPathString();
+        File f = new File(pathString);
+        assertTrue(f.exists());
+        String actualFileContent = Files.readString(Path.of(pathString));
+        assertEquals(FILE_CONTENT, actualFileContent);
     }
 
     @Test
@@ -38,5 +58,15 @@ class FileSystemStorageServiceTest {
 
     @Test
     void deleteAll() {
+    }
+
+    private void storeTestFile() {
+        byte[] content = FILE_CONTENT.getBytes(StandardCharsets.UTF_8);
+        MultipartFile result = new MockMultipartFile(FILENAME, FILENAME, CONTENT_TYPE, content);
+        storageService.store(result);
+    }
+
+    private String getPathString() {
+        return properties.getLocation() + "/" + FILENAME;
     }
 }
